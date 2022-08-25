5b7a23564a8c80e94a535874bd05209f77ca91d1
Muadz <muadzmohamad13@gmail.com>
2022-08-25 16:50:48 +0700

    Init Project. Register user endpoint

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bwastartup/handler"
+	"bwastartup/user"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	// router := gin.Default()
+	// router.GET("/", handler)
+	// router.Run()
+	dsn := "sql6514988:ev6vdZG56R@tcp(sql6.freesqldatabase.com:3306)/sql6514988?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+		return
+	}
+
+	userRepository := user.NewRepository(db)
+	userService := user.NewService(userRepository)
+	userHandler := handler.NewUserHandler(userService)
+
+	router := gin.Default()
+	api := router.Group("/api/v1")
+
+	api.POST("/users", userHandler.RegisterUser)
+
+	router.Run()
+}
+
+// func handler(c *gin.Context) {
+// 	dsn := "sql6514988:ev6vdZG56R@tcp(sql6.freesqldatabase.com:3306)/sql6514988?charset=utf8mb4&parseTime=True&loc=Local"
+// 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+// 	if err != nil {
+// 		log.Fatal(err.Error())
+// 		return
+// 	}
+
+// 	var users []user.User
+// 	db.Find(&users)
+
+// 	c.JSON(http.StatusOK, users)
+// }
